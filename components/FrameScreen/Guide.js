5df01426c4aaafd72647f79c43ababa7eebe860e
palpatine <20diobrando@gmail.com>
2021-11-12 17:06:19 +0700

    update using table component

--- components/FrameScreen/Guide.js
+++ components/FrameScreen/Guide.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Col, Row, Grid } from "react-native-easy-grid";
+import { Table, Row , Rows } from 'react-native-table-component';
 
 const Guide = (props) => {
     const data = props.route.params.data;
@@ -10,7 +10,6 @@ const Guide = (props) => {
         let moves = data;
 
         return (
-            <>
             <View style={styles.card}> 
                 <Text style={styles.title}> 
                     {title}
@@ -18,45 +17,33 @@ const Guide = (props) => {
                 <View style={styles.cardContent}>
                     {moves.map((item,i) => {
                         return (
-                            <Text style={styles.txtContent}>
+                            <Text key={i} style={styles.txtContent}>
                                 {item}
                             </Text>
                         )
                     })}
                 </View>
             </View>
-            </>
         )
     };
 
     const StandPunishLayout = ({title, data}) => {
+        let tableHead = ['Startup', 'Combo'];
+        let tableData = data;
+        const widthArr = [70,110,110,100];
 
         return (
-            <>
             <View style={styles.card}> 
                 <Text style={styles.title}> 
                     {title}
                 </Text>
-                <View style={styles.flexColumn}>
-                    <View style={styles.flexRow}>
-                        <Text style={styles.punishTitle}>Startup</Text>
-                        <Text style={styles.punishTitle}>Command</Text>
-                    </View>
-                   {data.map((item) => {
-                            return (
-                                <View style={styles.flexRow}>
-                                    {item.map((value,i) => {
-                                       return (
-                                        <Text style={styles.txtStandard}>{value}</Text>
-                                       )
-                                    })}
-                               </View>
-                            )
-                       })
-                   }
+                <View style={styles.tableContainer}>
+                    <Table >
+                        <Row data={tableHead} textStyle={styles.txtTableHeadPunish} widthArr={widthArr} />
+                        <Rows data={tableData} textStyle={styles.txtTableContent} widthArr={widthArr} />
+                    </Table>
                 </View>
             </View>
-            </> 
         )
     };
 
@@ -73,169 +60,56 @@ const Guide = (props) => {
             )
         });
 
-        return (
-            <>
-            <View style={styles.card}> 
-                <Text style={styles.title}> 
-                    {title}
-                </Text>
-                <Grid>
-                    <Col size={60}>
-                        <Text style={styles.punishTitle}> Startup</Text>
-                        {starter.map((item,i) => {
-                            return (
-                                <Row style={styles.flexWrap}>
-                                    {/* <Text key={i} style={styles.txtEnders}>
-                                        {item.join(' * ')}
-                                    </Text> */}
-                                    {item.map((value,j) => {
-                                        return (
-                                        <Text style={styles.txtStartup}>
-                                            {value}
-                                        </Text>
-                                        )
-                                    })}
-                                </Row>
-                            )
-                        })}
-                    </Col> 
-
-                    <Col size={40}>
-                        <Text style={styles.punishTitle}> Combo</Text>
-                        {combo.map((item,i) => {
-                            return (
-                                <Row>
-                                    <Text key={i} style={styles.txtCombo}>
-                                        {item}
-                                    </Text>
-                                </Row>
-                            )
-                        })}
-                    </Col> 
-
-                </Grid>
-            </View>
-            </> 
-        )
-    };
-
-    const ComboEnderLayout = ({title, data}) => {
-        let leftData = data.map((item) => {
-            return item[0];
-        });
-
-        let rightData = data.map((item) => {
-            return (
-                item.filter((value, i) => {
-                    if(i !== 0) return value;     
-                })
-            )
-        });
-
-        /* -- Same as below but functions
-        const dimensions = (arr) => {
-            return (
-                [arr.length,
-                arr.reduce((x, y) => Math.max(x, y.length), 0)]
-            )
+        let starterArrJoin = [];
+        for (let i = 0; i < starter.length; i++) {
+            starterArrJoin.push(starter[i].join())
         };
-        */
+ 
+        let tableHead = ['Startup', 'Combo'];
+        let tableData = [];
 
-        const dimensions = [
-            rightData.length,
-            rightData.reduce((x, y) => Math.max(x, y.length), 0)
-        ];
-
-        const display = () => {
-            let div = [];
-            for(var i = 0; i < dimensions[1]; i++) {
-                div.push(
-                    <Col size={23}>
-                        {rightData.map((item) => {
-                            return (
-                                <Text key={item} style={styles.txtEnders}>
-                                    {item[i]}
-                                </Text>
-                            )
-                        })}
-                    </Col>
-                )
-            }
-            return div;
+        for ( let i = 0; i < starterArrJoin.length; i++ ) {
+            tableData.push( [ starterArrJoin[i], combo[i] ] );
         };
-       
+
         return (
             <View style={styles.card}> 
                 <Text style={styles.title}> 
                     {title}
                 </Text>
-                <Grid>
-                    <Col size={30}>
-                        {leftData.map((item,i) => {
-                            return (
-                                <Text key={i} style={styles.txtEnders}>
-                                    {item}
-                                </Text>
-                            )
-                        })}
-                    </Col> 
-
-                    {display()}
-                    
-                </Grid>
+                <View style={styles.tableContainer}>
+                    <Table borderStyle={styles.tableBorder}>
+                        <Row data={tableHead} textStyle={styles.txtTableHead}/>
+                        <Rows data={tableData} textStyle={styles.txtTableContent}/>
+                    </Table>
+                </View>
             </View>
         )
     };
 
     const WallComboLayout = ({title, data}) => {
+        // Find max length of child element in 2d array
+        /*
         const dimensions = [
             data.length,
             data.reduce((x, y) => Math.max(x, y.length), 0)
         ];
+        */
+    
+        const tableData = data;
+        const widthArr = [80,100,100,100];
 
-        const display = () => {
-            let div = [];
-            for(var i = 0; i < dimensions[1]; i++) {
-                let size = null;
-                switch(i) {
-                    case 0: 
-                        size = 30;
-                        break;
-                    case 1:
-                        size = 25;
-                        break;
-                    case 2:
-                        size = 45;
-                        break;
-                    default:
-                }
-
-                div.push(
-                    <Col size={size}>
-                        {data.map((item) => {
-                            return (
-                                <Text key={item} style={styles.txtEnders}>
-                                    {item[i]}
-                                </Text>
-                            )
-                        })}
-                    </Col>
-                )
-            }
-            return div;
-        };
-        
         return (
-            <>
             <View style={styles.card}> 
                 <Text style={styles.title}> 
                     {title}
                 </Text>
-                <Grid>
-                    {display()}
-                </Grid>
+                <View style={styles.tableContainer}>
+                    <Table>
+                        <Rows data={tableData} textStyle={styles.txtTableContentAlt} widthArr={widthArr} />
+                    </Table>
+                </View>
             </View>
-           </>
         )
     };
 
@@ -248,14 +122,12 @@ const Guide = (props) => {
                 <KeyMovesLayout title="Whiff Punishers" data={data.whiffPunishers} />
                 <ComboLayout title="Standard Combos" data={data.standardCombos} />
                 <ComboLayout title="Small Combos" data={data.smallCombos} />
-                <ComboEnderLayout title="Combo Enders" data={data.comboEnders} />
+                <WallComboLayout title="Combo Enders" data={data.comboEnders} />
                 <WallComboLayout title="Wall Combos" data={data.wallCombos} />
-        
             </ScrollView>
         </SafeAreaView>
-    
     )
-}
+};
 
 const styles = StyleSheet.create({
     container: {
@@ -328,24 +200,52 @@ const styles = StyleSheet.create({
         marginLeft:27,
         fontFamily: 'ZenKurenaido'
     },
-    txtCombo : {
+    txtEnders : {
         color:'#ffff33',
         fontFamily: 'ZenKurenaido',
         marginVertical: 6,
-        marginHorizontal: 7,
+        marginHorizontal: 5,
     },
-    txtStartup: {
-        color:'#ffff33',
-        fontFamily: 'ZenKurenaido',
-        marginTop: 6,
-        marginLeft: 5,
+    txtTableHead: {
+        color: '#ffff33',
+        fontFamily: 'PatrickHand',
+        fontSize: 17,
+        textAlign: 'center'
     },
-    txtEnders: {
-        color:'#ffff33',
-        marginHorizontal: 7,
-        marginVertical: 7,
+    txtTableHeadPunish: {
+        color: '#ffff33',
+        fontFamily: 'PatrickHand',
+        fontSize: 17,
+        marginLeft: 5
+    },
+    txtTableContent : {
+        color: '#ffff33',
         fontFamily: 'ZenKurenaido',
+        marginHorizontal: 5,
+        marginVertical: 5
+    },
+    txtTableContentAlt: {
+        color: '#ffff33',
+        fontFamily: 'ZenKurenaido',
+        marginHorizontal: 8,
+        marginVertical: 8
+    },
+    tableContainer: { 
+        flex: 1, 
+        padding: 5, 
+        paddingBottom: 7,
+        backgroundColor: '#424242' 
+    },
+    tableHead: { 
+        height: 40, 
+        backgroundColor: '#f1f8ff',
+    },
+    tableBorder : {
+        borderWidth: 1, 
+        borderColor: '#c8e1ff'
     }
+   
+  
 });
 
 
@@ -425,3 +325,158 @@ const ComboLayout = ({title, data}) => {
         )
     })}
 </Col>  */
+
+/*
+const StandPunishLayout = ({title, data}) => {
+
+    return (
+        <>
+        <View style={styles.card}> 
+            <Text style={styles.title}> 
+                {title}
+            </Text>
+            <View style={styles.flexColumn}>
+                <View style={styles.flexRow}>
+                    <Text style={styles.punishTitle}>Startup</Text>
+                    <Text style={styles.punishTitle}>Command</Text>
+                </View>
+               {data.map((item) => {
+                        return (
+                            <View style={styles.flexRow}>
+                                {item.map((value,i) => {
+                                   return (
+                                    <Text key={i} style={styles.txtStandard}>{value}</Text>
+                                   )
+                                })}
+                           </View>
+                        )
+                   })
+               }
+            </View>
+        </View>
+        </> 
+    )
+};
+*/
+
+/* old layout need react native easy grid
+const WallComboLayout = ({title, data}) => {
+    const dimensions = [
+        data.length,
+        data.reduce((x, y) => Math.max(x, y.length), 0)
+    ];
+
+    const display = () => {
+        let div = [];
+        for(var i = 0; i < dimensions[1]; i++) {
+            let size = null;
+            switch(i) {
+                case 0: 
+                    size = 30;
+                    break;
+                case 1:
+                    size = 25;
+                    break;
+                case 2:
+                    size = 45;
+                    break;
+                default:
+            }
+
+            div.push(
+                <Col size={size}>
+                    {data.map((item) => {
+                        return (
+                            <Text key={item} style={styles.txtEnders}>
+                                {item[i]}
+                            </Text>
+                        )
+                    })}
+                </Col>
+            )
+        }
+        return div;
+    };
+    
+    return (
+        <>
+        <View style={styles.card}> 
+            <Text style={styles.title}> 
+                {title}
+            </Text>
+            <Grid>
+                {display()}
+            </Grid>
+        </View>
+       </>
+    )
+};
+
+const ComboEnderLayout = ({title, data}) => {
+        let leftData = data.map((item) => {
+            return item[0];
+        });
+
+        let rightData = data.map((item) => {
+            return (
+                item.filter((value, i) => {
+                    if(i !== 0) return value;     
+                })
+            )
+        });
+
+        //-- Same as below but functions
+        const dimensions = (arr) => {
+            return (
+                [arr.length,
+                arr.reduce((x, y) => Math.max(x, y.length), 0)]
+            )
+        };
+        
+
+       const dimensions = [
+        rightData.length,
+        rightData.reduce((x, y) => Math.max(x, y.length), 0)
+    ];
+
+    const display = () => {
+        let div = [];
+        for(var i = 0; i < dimensions[1]; i++) {
+            div.push(
+                <Col size={23}>
+                    {rightData.map((item,i) => {
+                        return (
+                            <Text key={i} style={styles.txtEnders}>
+                                {item[i]}
+                            </Text>
+                        )
+                    })}
+                </Col>
+            )
+        }
+        return div;
+    };
+   
+    return (
+        <View style={styles.card}> 
+            <Text style={styles.title}> 
+                {title}
+            </Text>
+            <Grid>
+                <Col size={30}>
+                    {leftData.map((item,i) => {
+                        return (
+                            <Text key={i} style={styles.txtEnders}>
+                                {item}
+                            </Text>
+                        )
+                    })}
+                </Col> 
+
+                {display()}
+                
+            </Grid>
+        </View>
+    )
+};
+*/
